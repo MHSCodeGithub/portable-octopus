b915c11567724d80120bd32b465969dbe53cab68
torinberger <beau.berghausen@education.nsw.gov.au>
2018-07-30 10:24:11 +1000

    fleshed out Island class

--- framework/classes/island.js
+++ framework/classes/island.js
@@ -6,10 +6,13 @@ const Objectable = require('./objectable');
  */
 
 class Island extends Objectable {
-  constructor(id, owner) {
-    super();
+  constructor(id, owner, kingdom) {
+    super(); // load Objectable class
     this.id = id; // unique integer id (starts at 0)
     this.owner = owner; // player class
+    this.kingdom = kingdom; // player class
+    // TODO: Work out resource system
+    // TODO: Add Island functions
   }
 }
 
