cfbdcd8f65115e0725c321f3f30ce248ee641d01
torinberger <beau.berghausen@education.nsw.gov.au>
2018-07-31 20:53:16 +1000

    changed function system to recursive to JSON child objects

--- framework/classes/objectable.js
+++ framework/classes/objectable.js
@@ -6,14 +6,16 @@
 
 class Objectable {
   constructor() {
-    this.toJSON = function () {
+    this.toJSON = function (target) {
       var jsonedObject = {};
-      for (var x in this) {
-
-        if (x === "toJSON" || x === "constructor" || typeof this[x] === 'function') {
+      for (var x in target) {
+        if(target[x].constructor !== Array && typeof target[x] == "object") {
+          jsonedObject[x] = this.toJSON(target[x]);
+        } else if (x === "toJSON" || x === "constructor" || typeof target[x] === 'function') {
           continue;
+        } else {
+          jsonedObject[x] = target[x];
         }
-        jsonedObject[x] = this[x];
       }
       return jsonedObject;
     }
