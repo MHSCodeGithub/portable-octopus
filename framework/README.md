4fe907a789d26c061675360b28518cbaf7ed45c3
torinberger <beau.berghausen@education.nsw.gov.au>
2018-07-31 16:40:03 +1000

    prevented anoying update bug + docs

--- framework/README.md
+++ framework/README.md
@@ -33,8 +33,6 @@
 ### Commodity
 `id` - **integer**, used to uniquely identify the commodity.
 
-`value` - **integer**, used to represent the economical value determined by supply and demand.
-
 `name` - **string**, name of commodity.
 
 `type` - **string**, type of commodity (mainly going to be used for sorting and UI stuff).
--- framework/classes/commodity.js
+++ framework/classes/commodity.js
@@ -9,7 +9,6 @@ class Commodity extends Objectable {
   constructor(id, name, type) {
     super();
     this.id = id; // unique integer id (starts at 0)
-    this.value = 0; // value of commodity
     this.name = name; // name of commodity
     this.type = type; // type of commodity (used for sorting)
     // TODO: Add Commodity functions
