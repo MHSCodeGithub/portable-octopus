9de87d694b834d6ec9b6eeeb8a8115a0ad0be5e0
torinberger <beau.berghausen@education.nsw.gov.au>
2018-08-15 17:33:27 +1000

    system for buying producers

--- api.js
+++ api.js
@@ -1,6 +1,16 @@
 
 const framework = require('./framework');
 
+function isItem(id) {
+  var items = framework.database.getItems();
+
+  for (var i = 0; i < items.length; i++) {
+    if(items[i].id == id) { return items[i]; }
+  }
+
+  return false;
+}
+
 exports.setup = function (app, gets) {
   app.get("/api/get/:type", function (req, res) {
     var type = req.params.type;
@@ -19,7 +29,63 @@ exports.setup = function (app, gets) {
     console.log(data);
 
     if(type == "buy-producer") {
-      res.send("ok")
+      var testAcc = new framework.Player(0, data.username, data.password, null, true);
+      if(testAcc.check()) {
+        var kingdom = testAcc.kingdom;
+        var current = kingdom.producers.length;
+        var result = isItem(data.target);
+
+        console.log(result);
+
+        if(result) {
+          switch (result.name) {
+            case "Wheat Farm":
+              var producer = new framework.producers.Farm(current, "wheat", 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Cotton Farm":
+              var producer = new framework.producers.Farm(current, "cotton", 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Cattle Farm":
+              var producer = new framework.producers.Farm(current, "cattle", 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Mill":
+              var producer = new framework.producers.Mill(current, 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Bakery":
+              var producer = new framework.producers.Bakery(current, 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Cotton Mill":
+              var producer = new framework.producers.CottonMill(current, 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+            case "Butchery":
+              var producer = new framework.producers.Butchery(current, 1, 1);
+
+              kingdom.producers.push(producer);
+              break;
+          }
+
+          console.log(kingdom.producers);
+
+          testAcc.update();
+          res.send("Updated!")
+        } else {
+          res.send({type: "error", data: "Invalid Item!"})
+        }
+      } else {
+        res.send({type: "error", data: "Invalid Username/Password"})
+      }
     } else if(type == "get-map") {
       var testAcc = new framework.Player(0, data.username, data.password, null, true);
       if(testAcc.check()) {
