03f34f8213301dfd65258ba871ea66e4a93e19d1
torinberger <beau.berghausen@education.nsw.gov.au>
2018-08-15 18:38:12 +1000

    better JSON algo

--- framework/classes/objectable.js
+++ framework/classes/objectable.js
@@ -6,14 +6,17 @@
 
 class Objectable {
   constructor() {
-    this.toJSON = function (target) {
-      var jsonedObject = {};
+    this.toJSON = function (target, type) {
+      if(type == undefined) { type = {}; }
+      var jsonedObject = type;
       for (var x in target) {
         try {
-          if((target[x].constructor !== Array && typeof target[x] == "object") || target[x].constructor === Array) {
+          if((target[x].constructor !== Array && typeof target[x] == "object")) {
             jsonedObject[x] = this.toJSON(target[x]);
           } else if (x === "toJSON" || x === "constructor" || typeof target[x] === 'function') {
             continue;
+          } else if(target[x].constructor === Array) {
+            jsonedObject[x] = this.toJSON(target[x], []);
           } else {
             jsonedObject[x] = target[x];
           }
