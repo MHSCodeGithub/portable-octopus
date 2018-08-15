7a31a8108d80fc40562c56ade8b7d15353c26f6f
torinberger <beau.berghausen@education.nsw.gov.au>
2018-08-15 18:38:30 +1000

    player class parses in pre-existing producers

--- framework/classes/player.js
+++ framework/classes/player.js
@@ -3,6 +3,8 @@ const sha256 = require('sha256');
 const Objectable = require('./objectable');
 const database = require('../../database');
 
+const framework = require('../');
+
 const Treasury = require('./treasury');
 const Producer = require('./producer');
 const Harbour = require('./harbour');
@@ -48,6 +50,36 @@ class Player extends Objectable {
           har.health = data.accounts[property].kingdom.harbour.health;
 
           var king = new Kingdom(data.accounts[property].kingdom.id, data.accounts[property].kingdom.name, tre, har);
+
+          for (var i = 0; i < data.accounts[property].kingdom.producers.length; i++) {
+            switch (data.accounts[property].kingdom.producers[i].type) {
+              case "farm":
+                var producer = new framework.producers.Farm(data.accounts[property].kingdom.producers[i].id, data.accounts[property].kingdom.producers[i].subType, 1, 1);
+
+                king.producers.push(producer);
+                break;
+              case "mill":
+                var producer = new framework.producers.Mill(data.accounts[property].kingdom.producers[i].id, 1, 1);
+
+                king.producers.push(producer);
+                break;
+              case "bakery":
+                var producer = new framework.producers.Bakery(data.accounts[property].kingdom.producers[i].id, 1, 1);
+
+                king.producers.push(producer);
+                break;
+              case "cotton_mill":
+                var producer = new framework.producers.CottonMill(data.accounts[property].kingdom.producers[i].id, 1, 1);
+
+                king.producers.push(producer);
+                break;
+              case "butchery":
+                var producer = new framework.producers.Butchery(data.accounts[property].kingdom.producers[i].id, 1, 1);
+
+                king.producers.push(producer);
+                break;
+            }
+          }
 
           this.kingdom = king;
         } catch (e) {
