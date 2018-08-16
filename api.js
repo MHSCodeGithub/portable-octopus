c6557aec056dd97f708fcc63704173c390197e5b
torinberger <beau.berghausen@education.nsw.gov.au>
2018-08-16 10:30:03 +1000

    added Gem Mine

--- api.js
+++ api.js
@@ -48,6 +48,11 @@ exports.setup = function (app, gets) {
 
               kingdom.producers.push(producer);
               break;
+            case "Gem Mine":
+              var producer = new framework.producers.Mine(current, "gem", Number(data.x), Number(data.y));
+
+              kingdom.producers.push(producer);
+              break;
             case "Wheat Farm":
               var producer = new framework.producers.Farm(current, "wheat", Number(data.x), Number(data.y));
 
