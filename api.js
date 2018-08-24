0732812509b5cf7abc803a838d271ef55912b656
Torin Berger <beau.berghausen@education.nsw.gov.au>
2018-08-24 13:46:09 +1000

    added wood farm

--- api.js
+++ api.js
@@ -194,6 +194,22 @@ exports.setup = function (app, gets) {
 
               kingdom.producers.push(producer);
               break;
+            case "Wood Farm":
+              var producer = new framework.producers.Farm(current, "wood", Number(data.x), Number(data.y));
+
+              var price = isItemName("Wood Farm").price;
+
+              console.log(price);
+
+              if(testAcc.kingdom.treasury.balance - price < 0) {
+                res.send({type: "error", data: "You do not have enough money!"});
+                return;
+              }
+
+              testAcc.charge(price);
+
+              kingdom.producers.push(producer);
+              break;
             case "Cattle Farm":
               var producer = new framework.producers.Farm(current, "cattle", Number(data.x), Number(data.y));
 
