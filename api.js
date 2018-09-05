6db27de9bd2a38b96164a68ad851c02bbf2e66b0
torinberger <beau.berghausen@education.nsw.gov.au>
2018-09-06 08:36:43 +1000

    fixed fullment bug + commenting

--- api.js
+++ api.js
@@ -332,22 +332,21 @@ exports.setup = function (app, gets) { // when the API is setup
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "sell-producer") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
+    } else if(type == "sell-producer") { // if the post type is sell producer
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
 
-        for (var i = 0; i < testAcc.kingdom.producers.length; i++) {
-          if(testAcc.kingdom.producers[i].id == data.target) {
-            var items = framework.database.getItems();
+        for (var i = 0; i < testAcc.kingdom.producers.length; i++) { // for each producer that the user owns
+          if(testAcc.kingdom.producers[i].id == data.target) { // if the producer matches the target's ID
+            var items = framework.database.getItems(); // get all producer templates
 
-            for (var k = 0; k < items.length; k++) {
+            for (var k = 0; k < items.length; k++) { // match the template with the target producer
               if(items[k].id == data.target) {
-                var price = (items[k].price * testAcc.kingdom.producers[i].level)/2;
+                var price = (items[k].price * testAcc.kingdom.producers[i].level)/2; // calaculate the sell price
 
-                testAcc.kingdom.producers[i].upgrade();
-                testAcc.charge(-price);
-                testAcc.kingdom.producers.splice(i, 1);
-                testAcc.update();
+                testAcc.charge(-price); // pay the user accordingly
+                testAcc.kingdom.producers.splice(i, 1); // remove the producer from their kingdom
+                testAcc.update(); // update the actions to the db
                 res.send("Updated!");
                 return;
               }
@@ -359,108 +358,114 @@ exports.setup = function (app, gets) { // when the API is setup
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "get-balance") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
-        res.send({data: testAcc.kingdom.treasury.balance});
+    } else if(type == "get-balance") { // if the post type is get balance
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
+        res.send({data: testAcc.kingdom.treasury.balance}); // send the user back their balance
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "create-order") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
-        delete data.password;
-        data.author = data.username;
+    } else if(type == "create-order") { // if the post type is create order
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validatet the account
+        delete data.password; // remove the users pasword
+        data.author = data.username; // rename data.username to data.author
         delete data.username;
-        data.id = getNextIDOfOrders();
 
-        data.fulfillment = 0;
+        data.id = getNextIDOfOrders(); // generate the ID of the order
 
-        framework.database.addOrder(data);
+        data.fulfillment = 0; // ste fulfillment to 0
+
+        framework.database.addOrder(data); // add order to db
         res.send("OK");
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "cancel-order") {
+    } else if(type == "cancel-order") { // if the psot type s cancel order
 
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
-        var db = framework.database.read();
-        for (var i = 0; i < db.orders.length; i++) {
-          if(db.orders[i].id == data.targetID) {
-            db.orders.splice(i, 1);
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
+        var db = framework.database.read(); // get the database
+
+        for (var i = 0; i < db.orders.length; i++) { // get the orders from the db
+          if(db.orders[i].id == data.targetID) { // if the target order matches the order's ID
+            db.orders.splice(i, 1); // remove the order from the db
           }
         }
 
-        framework.database.write(db);
+        framework.database.write(db); // save the db
         res.send("OK");
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "get-order") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
-        res.send(framework.database.getOrder(Number(data.id)));
+    } else if(type == "get-order") { // if the post type is get order
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
+        res.send(framework.database.getOrder(Number(data.id))); // send the specified order back
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "fulfill-order") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
-        var order = framework.database.getOrder(Number(data.id));
+    } else if(type == "fulfill-order") { // if the post type is fulfill order
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
+        var order = framework.database.getOrder(Number(data.id)); // get the target order
 
-        var otherAccount = framework.database.getAccount(order.author);
-        var other = new framework.Player(0, otherAccount.username, otherAccount.password, null, true);
-        if(other.check()) {
+        var otherAccount = framework.database.getAccount(order.author); // get the second account in the transaction
+        var other = new framework.Player(0, otherAccount.username, otherAccount.password, null, true); // create a second test account
+        if(other.check()) { // validate the account
 
-          console.log(order);
-          if(order.type == "sell") {
-            var balance = testAcc.kingdom.treasury.balance;
+          if(order.type == "sell") { // if the order type is sell
+            var balance = testAcc.kingdom.treasury.balance; // get the balance of the buyer
 
-            if(balance >= order.price*data.amount) {
-              if(other.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount >= Number(data.amount)) {
-                testAcc.charge(order.price*data.amount);
+            if(balance >= order.price*data.amount) { // check that their balance is larger than or equal to the transaction cost
+              if(other.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount >= Number(data.amount)) { // if the seller has the commodities needed to sell
+                testAcc.charge(order.price*data.amount); // apply transaction
                 other.pay(order.price*data.amount);
 
-                testAcc.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount += Number(data.amount);
+                testAcc.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount += Number(data.amount); // send commodities across
                 other.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount -= Number(data.amount);
                 res.send("SUCCESS!")
               } else {
                 res.send({type: "error", data: "Seller does not have enough commodities to sell!"})
+                return;
               }
             } else {
               res.send({type: "error", data: "Not Enough Money in Balance!"})
+              return;
             }
-          } else if(order.type == "buy") {
+          } else if(order.type == "buy") { // if the order type is buy
             var balance = other.kingdom.treasury.balance;
 
-            if(balance >= order.price*data.amount) {
-              if(testAcc.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount >= Number(data.amount)) {
-                other.charge(order.price*data.amount);
+            if(balance >= order.price*data.amount) { // check the buyer has enough money to buy the commodities
+              if(testAcc.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount >= Number(data.amount)) { // check the seller has enough commodities to sell
+                other.charge(order.price*data.amount); // apply transaction
                 testAcc.pay(order.price*data.amount);
 
-                other.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount += Number(data.amount);
+                other.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount += Number(data.amount); // send commodities across
                 testAcc.kingdom.harbour.commodities[framework.database.getCommodity(order.commodity).id].amount -= Number(data.amount);
                 res.send("SUCCESS!")
               } else {
                 res.send({type: "error", data: "You do not have enough commodities to sell!"})
+                return;
               }
             } else {
               res.send({type: "error", data: "Not Enough Money in Seller's Balance!"})
+              return;
             }
           }
-          var db = framework.database.read();
 
-          for (var i = 0; i < db.orders.length; i++) {
-            if(db.orders[i].id == data.id) {
-              db.orders[i].fulfillment += Number(data.amount);
-              if(db.orders[i].fulfillment == Number(db.orders[i].amount)) {
-                db.orders.splice(i, 1);
+          var db = framework.database.read(); // get the db
+
+          for (var i = 0; i < db.orders.length; i++) { // for each order in db
+            if(db.orders[i].id == data.id) { // check that the target order matches the order
+              db.orders[i].fulfillment += Number(data.amount); // fulfill the order
+              if(db.orders[i].fulfillment == Number(db.orders[i].amount)) { // check whether the order is complete and can be removed
+                db.orders.splice(i, 1); // remove the order
               }
             }
-          } framework.database.write(db);
+          } framework.database.write(db); // update the db
 
-          other.update()
+          other.update() // update the accounts to the db
           testAcc.update()
         } else {
           res.send({type: "error", data: "Author of Order Does Not Exist!"})
@@ -468,14 +473,13 @@ exports.setup = function (app, gets) { // when the API is setup
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "get-producer") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
+    } else if(type == "get-producer") { // if the post type is get producer
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
 
-
-        for (var i = 0; i < testAcc.kingdom.producers.length; i++) {
-          if(testAcc.kingdom.producers[i].id == data.target) {
-            res.send(testAcc.toJSON(testAcc).kingdom.producers[i]);
+        for (var i = 0; i < testAcc.kingdom.producers.length; i++) { // go through the user's producers
+          if(testAcc.kingdom.producers[i].id == data.target) { // if the target producer matches the producer
+            res.send(testAcc.toJSON(testAcc).kingdom.producers[i]); // send JSON version of the producer
             return;
           }
         }
@@ -484,19 +488,19 @@ exports.setup = function (app, gets) { // when the API is setup
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
-    } else if(type == "get-yeild") {
-      var testAcc = new framework.Player(0, data.username, data.password, null, true);
-      if(testAcc.check()) {
+    } else if(type == "get-yeild") { // if the post type is get yeild
+      var testAcc = new framework.Player(0, data.username, data.password, null, true); // create a test account
+      if(testAcc.check()) { // validate the account
 
 
-        for (var i = 0; i < testAcc.kingdom.producers.length; i++) {
-          if(testAcc.kingdom.producers[i].id == data.target) {
-            res.send({val: testAcc.kingdom.producers[i].yeild()});
-            return;
+        for (var i = 0; i < testAcc.kingdom.producers.length; i++) { // for each producer the user owns
+          if(testAcc.kingdom.producers[i].id == data.target) { // check if producer is target
+            res.send({val: testAcc.kingdom.producers[i].yeild()}); // send the yeild of the producer
+            return; // prevent double sending
           }
         }
 
-        res.send({type: "error", data: "Request for non-existant producer, please contact developers!"});
+        res.send({type: "error", data: "Request for non-existant producer, please contact developers!"}); // will run if no producer is matched
       } else {
         res.send({type: "error", data: "Invalid Username/Password"})
       }
